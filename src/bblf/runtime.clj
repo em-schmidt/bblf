4f84bbdf5924dec778c4122483d15185b9166de7
eric m. schmidt <eric@spaceba.by>
2024-01-04 08:52:24 -0500

    wip: runtime init and error handling

--- src/bblf/runtime.clj
+++ src/bblf/runtime.clj
@@ -1,5 +1,6 @@
 (ns bblf.runtime
   (:require [babashka.http-client :as http]
+            [clojure.string :as str]
             [cheshire.core :as json]
             [taoensso.timbre :as log]))
 
@@ -10,40 +11,41 @@
   (http/get (str "http://" runtime-api "/runtime/invocation/next") {:timeout lambda-timeout}))
 
 (defn- send-runtime-response
-  [runtime-api request-id body]
-  (http/post (str "http://" runtime-api "/runtime/invocation/" request-id "/response") 
+  [runtime-api request-id response-type body]
+  (http/post (str "http://" runtime-api "/runtime/invocation/" request-id "/" response-type) 
              {:headers {"Content-type" "application/json"}
               :body (json/generate-string body)}))
 
-(defn- send-runtime-error
-  [runtime-api request-id body]
-  (http/post (str "http://" runtime-api "/runtime/invocation/" request-id "/error") 
-             {:headers {"Content-type" "application/json"}
-              :body (json/generate-string body)}))
-
-(defn- call-handler
-  [f req]
-  (log/info "calling handler" {:handler f
-                               :req req})
-  true)
+(defn- get-handler
+  [f]
+  (let [[hns hfn] (str/split f #"/")]
+    (log/info "finding handler" {:function hfn
+                                 :ns hns})
+    (let [fname (or hfn "-main")
+          fqfn (symbol (str hns "/" fname))]
+      (require (symbol hns))
+      (resolve fqfn))))
 
 (defn -main [_]
   (let [handler (System/getenv "_HANDLER")
         runtime-api (str (System/getenv "AWS_LAMBDA_RUNTIME_API") "/2018-06-01")]
       (log/info "initializing runtime handler loop"{:handler handler
                                                     :runtime-api runtime-api})
-      (loop [req (get-runtime-event runtime-api)]
-        (let [request-id (-> req :headers (get "lambda-runtime-aws-request-id"))
-              status (-> req :status)]
-          (log/info "got request" {:request-id request-id
-                                   :status status})
-          (try (send-runtime-response 
-                 runtime-api
-                 request-id 
-                 (call-handler handler req))
-               (catch Exception e
-                 (send-runtime-error runtime-api request-id {:id request-id :error (.getMessage e) :stacktrace (mapv str (.getStackTrace e))}))))
-        (recur (get-runtime-event runtime-api)))))
+      (let [handler-fn (get-handler handler)]
+        (loop [req (get-runtime-event runtime-api)]
+         (let [request-id (-> req :headers (get "lambda-runtime-aws-request-id"))
+               status (-> req :status)]
+           (log/info "got request" {:request-id request-id
+                                    :status status})
+           (try (send-runtime-response 
+                  runtime-api
+                  request-id 
+                  "response"
+                  (handler-fn req))
+                (catch Exception e
+                  (send-runtime-response runtime-api request-id "error" {:id request-id :error (.getMessage e) :type (-> e .getClass .getName) :stacktrace (mapv str (.getStackTrace e))}))))
+         (recur (get-runtime-event runtime-api))))))
 
-(comment)
-  
+(comment
+  (resolve (symbol "bblf.runtime/-main")))
+
